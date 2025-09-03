f30d6d932e07aaed092e4375cd111330df440107
SurajKale01 <surajkalecsit@gmail.com>
2025-09-03 16:33:47 +0530

    added the db package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,12 +2,39 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { authMiddleware } from "./middlewares/auth.middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
 
 const app = express();
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
+  // db call
+
+  res.json({
+    userId: "123",
+  });
+});
 
 app.post("/signin", (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
   // db call
   const userId = 1;
   const token = jwt.sign(
@@ -23,6 +50,14 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room", authMiddleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
   // db call
 
   res.json({
